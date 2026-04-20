e007c2c944276ac03c5b6da8d84a2a79daf76f4a
phuoc147 <phuoc.hatruong@hcmut.edu.vn>
2026-04-20 23:31:04 +0700

    feat: add api

--- src/app/pn/layout.tsx
+++ src/app/pn/layout.tsx
@@ -1,17 +1,10 @@
 import type { ReactNode } from "react";
 import { redirect } from "next/navigation";
-import { getServerSessionUser } from "@/src/features/auth/lib";
 
 export default async function PartnerLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  // const user = await getServerSessionUser();
-
-  // if (!user || user.role !== "partner") {
-  //   redirect("/login");
-  // }
-
   return <>{children}</>;
 }
